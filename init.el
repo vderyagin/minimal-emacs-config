482b05513f0ad43922d5e275c5b521d30072b2bc
Victor Deryagin <vderyagin@gmail.com>
2013-03-14 10:48:51 +0200

    turn on sh-mode when used by zsh' edit-command-line

--- init.el
+++ init.el
@@ -37,6 +37,7 @@
 (add-to-list 'auto-mode-alist '("\\.\\(rb\\|ru\\|builder\\|rake\\|thor\\|gemspec\\)\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\(rake\\|thor\\|guard\\|gem\\|cap\\|vagrant\\)file\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("^/tmp/zshec" . sh-mode))
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
